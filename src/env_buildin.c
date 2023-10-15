a6d16f0b68d69ab1b2abcb9df305281f283b53a9
aamaral-42 <131622188+aamaral-42@users.noreply.github.com>
2023-10-15 21:22:56 +0100

    with options included

--- src/env_buildin.c
+++ src/env_buildin.c
@@ -1,29 +1,59 @@
 #include <stdio.h>	// printf
+#include <stdlib.h>	// exit
+#include <errno.h>	// errno
+#include <limits.h>
+#include <unistd.h>
 
-
-
-/*it will be substituted by the env_buildin(char **envp)
+/*
 I've saw our colleagues using different solutions, more complicated ones
 based on youtube video of code vault about environment variables
 at the end use the following command to compare the differences between the original env and the env buildin
 _= is a special bash parameter https://www.cherryservers.com/blog/how-to-set-list-and-manage-linux-environment-variables
 declare and set commands include shell functions that we are not taking into account for this simplified shell
 */
-int	main(int argc, char **argv, char **envp) // not all OS allow to use envp. it is null terminated containing the envvariomental variables as string
+
+
+/*
+Tests to check adherence to original bash
+
+in minishell env doesn't accept no options nor arguments
+
+- run	./a.out env ; $?
+- run	./a.out env "more arguments" ; $?
+
+*/
+
+
+int	exit_status; // in the original will be a global variable
+
+void	env_buildin(int argc, char **argv, char **env_copy)
 {
-	(void)argc;	// in the buildin function they will not be used
-	(void)argv;	// in the buildin function they will not be used
-//	int	exit_status; // in the original will be a global variable
-	int	i = 0;
-	char	*env_var = envp[0]; // there is always at least one pointer; the NULL since envp is nullterminated
+	int	i = 0;			// index variable to goes through the arrays of pointers of each envp string pointer 
+	char	*env_var = envp[0];	// declare and initialize there is always at least one pointer; the NULL since envp is nullterminated by standard request
+
+				
+	// run grep ARG_MAX /usr/include/linux/limits.h  ; getconf ARG_MAX https://unix.stackexchange.com/questions/120642/what-defines-the-maximum-size-for-a-command-single-argument
 	
+	(void) argv;			// instead use argc but must be a parameter, we can also count locally argv string
+	if(argc > 2)			// a test number, it will be changed to 1 in original minishell 
+	{
+		errno = EINVAL;		// assign the error number 22 ( other possibility - E2BIG 7 see link above about ARG_MAX)for perror function()  on CLI run errno -l to see all the errors list; I would prefer too many arguments error but not implemented
+		perror("minishell buildin specific");	// print the errono with a personal message also
+		exit_status = 2;	// find the best exit status number
+		exit(exit_status);
+	}
 	while (env_var)
 	{
 		printf("%s\n", env_var);
 		i++;
 		env_var = envp[i];
 	}
-//	exit_status = 0; // gives me an error that i don't understand why, since I initialize it at line 7  
+	exit_status = EXIT_SUCCESS;
+}
+
+int	main(int argc, char **argv, char **envp) // not all OS allow to use envp. it is null terminated containing the envvariomental variables as string or we can use the external variable environ
+{
+	env_buildin(argc, argv, envp);
 //	it will never give error since is nullterminated and will give null at the worst case cenario
 	return 0;	// in the original function no need to return it will be void
 }
--- src/pwd_buildin.c
+++ src/pwd_buildin.c
@@ -5,28 +5,50 @@
 #include <string.h>	// strerror
 #include <stdlib.h>	// EXIT_FAILURE EXIT_SUCCESS
 
-/*to be substituted by the final function prototype
-void	pwd_buildin(t_list words) or to build the binary and give the path to it*/
-int	main(void)
+int	exit_status;
+
+int	is_option(char *str)
 {
-	int	exit_status;
+	if (!str)
+		return (0);
+	if (*str == '-')
+	{
+		while (*(++str) == 'n')
+			;
+//		printf("%c\n", *str);
+		if (!*str)	
+			return (write(1, "option -n\n", 10));
+		else
+			return (write(1, "option other than -n\n", 21));
+	}
+	return (0);
+	
+}
+
+void	pwd_buildin(int argc, char **argv, char **env_copy)
+{
 	/*function just to confirm how long the array can hold	 
-	  stackoverflow question 9449241 where is PATH_MAX defined in Linux*/
-	printf("max char that a path can hold: %d\n", PATH_MAX);
+	  stackoverflow question 9449241 where is PATH_MAX defined in Linux
+	printf("max char that a path can hold: %d\n", PATH_MAX);*/
 
 	char	curr_work_dir[PATH_MAX];
 
+	
+	while (*argv)
+		if(is_option(*(++argv)))			// a test number, it will be changed to 1 in original minishell ; pwd only accepts options man page, any other argument will be ignored
+		{
+			errno = EINVAL;		// assign the error number 22 ( other possibility - E2BIG 7 see link above about ARG_MAX)for perror function()  on CLI run errno -l to see all the errors list; I would prefer too many arguments error but not implemented
+			perror("minishell buildin specific");	// print the errono with a personal message also
+			exit_status = 2;	// find the best exit status number  https://www.baeldung.com/linux/standard-error-eodes#:~:text=This%20error%20code%20means%20that%20the%20argument%20passed,a%20valid%20and%20supported%20argument%20to%20the%20operation.
+			exit(exit_status);
+		}
 	if(getcwd(curr_work_dir, PATH_MAX) == NULL) // man getpwd return value on failure NULL pointer and errno is set
 	{
 	/*use panic to print the error message
 	panic("pwd"); */
-	perror("pwd: ");
-	//just to check if the errno ATTENTION: errno and exit code DON'T match https://www.gnu.org/software/libc/manual/html_node/Exit-Status.html
-	strerror(errno);
-	
-	exit_status = 127;  // https://tldp.org/LDP/abs/html/exitcodes.html   https://itsfoss.com/linux-exit-codes/
-	/*just to see if the errno functions. check if in real bash exit code is the same*/
-	printf("%d", exit_status);
+		perror("pwd error:");	//just to check if the errno ATTENTION: errno and exit code DON'T match https://www.gnu.org/software/libc/manual/html_node/Exit-Status.html
+
+		exit_status = 2;  // https://tldp.org/LDP/abs/html/exitcodes.html   https://itsfoss.com/linux-exit-codes/
 	}	
 	
 	else
@@ -34,4 +56,11 @@ int	main(void)
 		printf("%s\n", curr_work_dir);	// on success returns a pointer to string containing the path
 		exit_status = EXIT_SUCCESS;	// man getpwd on success returns the pointer to the array containing the path
 	}
+
 }
+
+int	main(int argc, char **argv, char **envp)
+{
+	pwd_buildin(argc, argv, envp);
+}
+
